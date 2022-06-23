d4c16b33747e16d65bb652315541fbcddee5f1ce
Aske Færch Christensen <askefc@gmail.com>
2022-06-23 10:42:10 +0200

    Update
    Use newer syntax.
    Fix sending socket library.

--- client/sockethandling.js
+++ client/sockethandling.js
@@ -1,117 +1,117 @@
-var socket = io();
+const socket = io();
 
-socket.on('newChat', function(msg){
-  var snake = getSnake(msg.id);
-  if(snake){
+socket.on('newChat', (msg) => {
+  const snake = getSnake(msg.id);
+  if (snake) {
     snake.say(msg.message);
   }
 });
 
-socket.on('gameOver', function(msg) {
-  var players = msg.players;
-  var winner = msg.winner;
+socket.on('gameOver', (msg) => {
+  const players = msg.players;
+  const winner = msg.winner;
   game.gameWon(players,winner);
 });
 
-socket.on('gameMode', function(msg) {
-  var mode = msg.mode;
-  var settings = msg.gameSettings;
+socket.on('gameMode', (msg) => {
+  const mode = msg.mode;
+  const settings = msg.gameSettings;
   game.setupBoard(settings.width, settings.height, settings.winLength);
   game.changeMode(mode);
 });
 
-socket.on('serverTick', function(msg){
-  var snakes = msg.snakes;
+socket.on('serverTick', (msg) => {
+  const snakes = msg.snakes;
   updateSnakes(snakes);
   game.move();
 });
 
-socket.on('warpSnake', function(msg){
-  var snake = getSnake(msg.id);
+socket.on('warpSnake', (msg) => {
+  const snake = getSnake(msg.id);
   snake.loadWarp(msg.segments);
 });
 
-socket.on('loseHead', function(msg){
-  var snake = getSnake(msg.id);
+socket.on('loseHead', (msg) => {
+  const snake = getSnake(msg.id);
   snake.loseHead();
 });
 
-socket.on('loseSegment', function(msg) {
-  var snake = getSnake(msg.id);
-  var x = parseInt(msg.x);
-  var y = parseInt(msg.y);
-  var showParticle = msg.showParticle;
+socket.on('loseSegment', (msg) => {
+  const snake = getSnake(msg.id);
+  const x = parseInt(msg.x);
+  const y = parseInt(msg.y);
+  const showParticle = msg.showParticle;
   snake.loseSegment(x, y, showParticle);
 });
 
-socket.on('gameSetup', function(msg){
-  var width = parseInt(msg.width);    // board width
-  var height = parseInt(msg.height);  // board height
-  var id = parseInt(msg.id);          // player id
-  var apples = msg.apples;            // apples already in play
-  var snakes = msg.snakes;            // snakes already in play
-  var winLength = msg.winLength;      // snake length needed to win
+socket.on('gameSetup', (msg) => {
+  const width = parseInt(msg.width);    // board width
+  const height = parseInt(msg.height);  // board height
+  const id = parseInt(msg.id);          // player id
+  const apples = msg.apples;            // apples already in play
+  const snakes = msg.snakes;            // snakes already in play
+  const winLength = msg.winLength;      // snake length needed to win
   game.setup(width, height, id, apples, snakes, winLength);
 });
 
-socket.on('snakeEat', function(msg){
-  var id = msg;
-  var snake = getSnake(id);
+socket.on('snakeEat', (msg) => {
+  const id = msg;
+  const snake = getSnake(id);
   snake.eat();
 });
 
-socket.on('addApple', function(msg){
-  var x = parseInt(msg.x);
-  var y = parseInt(msg.y);
-  var id = parseInt(msg.id);
+socket.on('addApple', (msg) => {
+  const x = parseInt(msg.x);
+  const y = parseInt(msg.y);
+  const id = parseInt(msg.id);
   game.addApple(x,y,id);
 });
 
-socket.on('removeApple', function(id){
+socket.on('removeApple', (id) => {
   game.removeApple(id);
 });
 
-socket.on('addBomb', function(msg){
-  var x = parseInt(msg.x);
-  var y = parseInt(msg.y);
-  var id = parseInt(msg.id);
-  var color = msg.color;
+socket.on('addBomb', (msg) => {
+  const x = parseInt(msg.x);
+  const y = parseInt(msg.y);
+  const id = parseInt(msg.id);
+  const color = msg.color;
   game.addBomb(x,y,id,color);
 });
 
-socket.on('removeBomb', function(id){
+socket.on('removeBomb', (id) => {
   game.removeBomb(id);
 });
 
-socket.on('message', function(msg){
+socket.on('message', (msg) => {
   console.log(msg.content);
 });
 
-socket.on('spawnSnake', function(msg){
-  var id = parseInt(msg.id);
-  var x = parseInt(msg.x);
-  var y = parseInt(msg.y);
-  var color = msg.color;
-  var direction = msg.direction;
-  var length = msg.length;
-  var name = msg.name || "no_name"
+socket.on('spawnSnake', (msg) => {
+  const id = parseInt(msg.id);
+  const x = parseInt(msg.x);
+  const y = parseInt(msg.y);
+  const color = msg.color;
+  const direction = msg.direction;
+  const length = msg.length;
+  const name = msg.name || "no_name";
   game.addSnake(id, x, y, color, direction, length, name);
 });
 
 
 // When another player disconnects...
-socket.on('playerDisconnect', function(msg){
+socket.on('playerDisconnect', (msg) => {
   game.removePlayer(msg.id);
 });
 
 // killSnake - where does this come from
-socket.on('killSnake', function(msg){
-  var id = msg.id;
-  var x = msg.x;
-  var y = msg.y;
-  var type = msg.type;
-  var snake = getSnake(msg.id);
-  if(snake){
+socket.on('killSnake', (msg) => {
+  const id = msg.id;
+  const x = msg.x;
+  const y = msg.y;
+  const type = msg.type;
+  const snake = getSnake(msg.id);
+  if (snake) {
     snake.die(x,y,type);
-  }
+  };
 });
--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 require('./sockets')(app);
 
-var path = require('path');
+const path = require('path');
 
 app.use(express.static(path.join(__dirname,'..','client')));
 app.use("/touchpad", express.static(path.join(__dirname,'..','touchpad')));
 app.use("/sounds", express.static(path.join(__dirname,'..','sounds')));
 
-app.get('/socket.io.js', function(req, res) {
-  res.sendFile('socket.io.js', {
-  	root: path.join(__dirname, '..', 'node_modules', 'socket.io-client')
+app.get('/socket.io.js', (req, res) => {
+  res.sendFile('socket.io.min.js', {
+  	root: path.join(__dirname, '..', 'node_modules', 'socket.io-client', 'dist')
   });
 });
